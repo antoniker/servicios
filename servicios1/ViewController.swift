58c8bada52da8ba05de930fc45fe885ef613d761
antoniker <jos_mustang@hotmail.com>
2015-12-18 23:35:48 -0600

    avance

--- servicios1/ViewController.swift
+++ servicios1/ViewController.swift
@@ -8,10 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var ingresarISBN: UITextField!
+    @IBOutlet weak var resultadoISBN: UITextView!
+   
+    func sincrono(){
+        let urls = "http://dia.ccm.itesm.mx"
+        let url = NSURL(string: urls)
+        let datos:NSData? = NSData(contentsOfURL: url!)
+        let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+        print(texto!)
+    }
+    
+     func asincrono(){
+        
+        if let text = ingresarISBN.text {
+            let url = NSURL(string : "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:\(text)")
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+                print (NSString(data: data!,
+                    encoding: NSUTF8StringEncoding))
+                let impresion = NSString(data: data!,
+                    encoding: NSUTF8StringEncoding)
+                dispatch_async(dispatch_get_main_queue()) {  // GCD
+                    self.resultadoISBN.text = impresion as! String // might need a [weak self]
+                }
+            }
+            task.resume()
+        }
+}
+    
+    
+    @IBAction func btnLimpiar(sender: AnyObject) {
+        ingresarISBN.text = ""
+        resultadoISBN.text = ""
+    }
+    
+
+    @IBAction func realizarActividad(sender: AnyObject) {
+          asincrono()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+     
+        ingresarISBN.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
